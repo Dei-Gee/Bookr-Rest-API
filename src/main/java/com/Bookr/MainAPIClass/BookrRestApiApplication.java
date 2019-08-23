e703a27bab5c71ef6fc3d405a3946c798b11e7d4
Dei-Gee <deji2109@gmail.com>
2019-08-23 05:29:12 -0700

    Added scans, WebMVC Bean and CORS mapping to main class

--- src/main/java/com/Bookr/MainAPIClass/BookrRestApiApplication.java
+++ src/main/java/com/Bookr/MainAPIClass/BookrRestApiApplication.java
@@ -2,12 +2,55 @@ package com.Bookr.MainAPIClass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 
+ * @author Deji9000
+ *
+ */
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.Bookr.Controllers", "com.Bookr.Services"})
+@EntityScan("com.Bookr.Entities")
+@EnableJpaRepositories("com.Bookr.Boundaries")
 public class BookrRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookrRestApiApplication.class, args);
 	}
+	
+	@Bean
+	   public WebMvcConfigurer corsConfigurer() {
+	      return new WebMvcConfigurer() {
+	         @Override
+	         public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/api/users").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/users/all").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/users/all_roles").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/users/{user_id}").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/users/create_user").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/users/login").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/users/delete/{user_id}").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/reservations").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/reservations/all").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/reservations/{reservation_id}").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/reservations/create_reservation").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/rooms").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/rooms/all").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/rooms/{room_id}").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/requests").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/requests/create_request").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/requests/all").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/requests/{request_id}").allowedOrigins("http://localhost:8000");
+	            registry.addMapping("/api/reservations/update/{res_number}/{new_start}/{new_end}").allowedOrigins("http://localhost:8000");
+	         }
+	      };
+	   }
 
 }
