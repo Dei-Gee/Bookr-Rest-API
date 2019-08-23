9b54a382fce6656e9859d5660303e56277b01b85
Dei-Gee <deji2109@gmail.com>
2019-08-23 04:41:07 -0700

    initial commit

---
+++ src/main/java/com/Bookr/MainAPIClass/BookrRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.Bookr.MainAPIClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookrRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookrRestApiApplication.class, args);
+	}
+
+}
